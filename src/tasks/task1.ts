e14faa00129f9bff4e1e3000769b93c26901c4e6
Siddhant Pandey <pandeysiddhant21@gmail.com>
2024-03-27 16:18:50 +0530

    added task code files

---
+++ src/tasks/task1.ts
@@ -0,0 +1,6 @@
+// Task 1 : You have 3 timestamps for list of users along with
+// the device details : logged_in, logged_out, and lastSeenAt
+// (for the device during the log in period).
+// Please note: Users can log in from multiple devices, users
+// are generally logged in for a longer duration of time 6
+// months or so.
